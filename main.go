648726640e2350e43116a1f61ecdecc33959f527
Alexey Matvievsky <alexey_matvievsky@epam.com>
2022-11-29 16:24:03 +0400

    Array vs slice

--- main.go
+++ main.go
@@ -3,24 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(findMedianSortedArrays(
-		[]int{1, 2},
-		[]int{3, 4},
-	))
+	arr := []int{1, 2, 3}
+	test(arr)
+	fmt.Println(arr)
+	slc := make([]int, 0, 3)
+	slc = append(slc, 1, 2, 3)
+	test(slc)
+	fmt.Println(slc)
 }
 
-func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	c := make(chan float64, 2)
-	f := func(nums []int, c chan float64) {
-		length := len(nums)
-		i := length / 2
-		if length%2 == 0 {
-			c <- float64(nums[i-1]+nums[i]) * 0.5
-			return
-		}
-		c <- float64(nums[i])
-	}
-	go f(nums1, c)
-	go f(nums2, c)
-	return (<-c + <-c) * 0.5
+func test(nums []int) {
+	nums[1] = 3
 }
